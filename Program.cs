3e801917926c4a718bef753e3006fdc7b7d0c204
Bhagya14 <45635523+Bhagya14@users.noreply.github.com>
2018-12-31 00:01:04 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Console_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            int x1 = 100;
+            int x2 = 200;
+            int x3 = 300;
+            list.Add(x1);
+            list.Add(x2);
+            list.Add(x3);
+            int c = list.Count;
+            list.RemoveAt(200);
+            list.Remove(200);
+            list.RemoveRange(1, 2);
+            c = list.Count;
+            //int y1 = Convert.ToInt32(list[0]);
+            int y1 = Convert.ToInt32(list[1]);
+            Console.WriteLine(y1);
+            Console.ReadLine();
+        }
+    }
+}
